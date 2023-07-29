23fe670d766b6ddf917d99f1f2a4251dea132f99
Jaisree2425 <140824710+Jaisree2425@users.noreply.github.com>
2023-07-29 12:27:14 +0530

    Add files via upload

---
+++ Mini Calculator.py
@@ -0,0 +1,27 @@
+print("!!!Mini Calculator!!!")
+
+while True:
+
+     num1 = float(input("enter first number here: "))
+     num2 = float(input("enter second number here: "))
+
+
+     print("press 1 for addition \npress 2 for subtraction \npress 3 for multiplication \npress 4 for division")
+
+     choice = int(input("enter your choice from 1-4: "))
+
+     if choice == 1:
+        print ("The addition of given two numbers is",num1 + num2)
+     elif choice == 2:
+        print ("The subtraction of given two numbers is",num1 - num2)
+     elif choice == 3:
+        print ("The multiplication of given two numbers is",num1 * num2)
+     elif choice == 4:
+        print ("The division of given two numbers is",num1 / num2)
+     else:
+        print ("Invalid Input")
+     v = input("Quit: ")
+     if v == "q":
+          exit()
+     
+    
